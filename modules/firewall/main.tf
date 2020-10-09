70f6026b21cb00b8793771b18912f795d33c5d7a
vponnam <ponnamchaitanya@gmail.com>
2020-10-08 20:36:27 -0400

    update firewall tag

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -18,7 +18,7 @@ locals {
 }
 
 resource "google_compute_firewall" "allow-http" {
-  name    = "${local.network}-allow-http"
+  name    = "${local.network}-allow-http-calls"
   network = "${local.network}"
   project = "${var.project}"
 
